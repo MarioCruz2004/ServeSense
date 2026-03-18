e92fe815c2bf25d3a982e4c6f85bedb6566990f0
MarioCruz2004 <macruz@andrew.cmu.edu>
2026-03-18 12:48:51 -0400

    Added updated arduino code for the feather

--- sampling_batching.ino
+++ sampling_batching.ino
@@ -12,6 +12,7 @@ const int BUTTON_PIN = 7; // D7 is the User Button which we'll be using for cali
 const int INTERRUPT_PIN = 11;
 volatile bool triggerCalibration = false;
 volatile bool triggerRead = false;
+volatile bool triggerSend = false;
 unsigned long lastInterruptTime = 0;
 
 ICM42688_FIFO IMU(SPI, 10);
@@ -23,9 +24,47 @@ struct __attribute__((packed)) imu_packet_t {
   unsigned long time_stamp;
 };
 
+// Defining some macros and globals for data buffer 
+#define SAMPLE_SIZE sizeof(imu_packet_t)
+#define SAMPLES_PER_PACKET 8
+uint8_t dataBuffer[SAMPLE_SIZE * SAMPLES_PER_PACKET];
+volatile int sampleCount = 0;
+
 unsigned long starting_time;
 unsigned long last_sample_micros = 0;
 const unsigned long intervalMicros = 200000 ; 
+
+void setupBLE(){
+  Serial.println("Configuring Bluetooth");
+  // Initializing Bluefruit Hardware Stack
+  Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
+  Bluefruit.begin();
+  // Requesting maximum bandwidth 247 bytes per transmission
+  // The laptop requires a bit of time to setup before max bandwidth is available
+  // So we're doing a call back function which will trigger max bandwidth after connection is setup
+  Bluefruit.Periph.setConnectCallback(connect_callback);
+  // Setting Name
+  Bluefruit.setName("Feather");
+  // Setting up service
+  // increasing bluetooth transmission speed in the air
+  imuService.begin();
+
+  // Setup Characteristic: 
+  // We set MaxLen to 512 so we can pack multiple 28-byte samples.
+  txCharacteristic.setProperties(CHR_PROPS_NOTIFY);
+  txCharacteristic.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  txCharacteristic.setMaxLen(512); 
+  txCharacteristic.begin();
+
+  rxCharacteristic.setProperties(CHR_PROPS_WRITE | CHR_PROPS_WRITE_WO_RESP);
+  rxCharacteristic.setPermission(SECMODE_OPEN, SECMODE_OPEN); // Open for writing
+  rxCharacteristic.setFixedLen(1); // Since we only send 'g'
+  
+  // 3. Optional: Add a callback to react to the write immediately
+  rxCharacteristic.setWriteCallback(receive_callback);
+  
+  rxCharacteristic.begin();
+}
 
 void setup() {
   Serial.begin(115200);
@@ -43,50 +82,15 @@ void setup() {
   IMU.enableDataReadyInterrupt();
   Serial.println("IMU has begun...");
   
-
-  Serial.println("Setting up the button for calibration");
-
+  // Serial.println("Setting up the button for calibration");
   // The D7 button on the Feather connects to GND when pressed
   // pinMode(BUTTON_PIN, INPUT_PULLUP);
   // attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), handleButtonPress, FALLING);
 
-  Serial.println("Configuring Bluetooth Settings");
-  // Initialize Bluefruit
-  // Bluefruit.begin();
-  // Bluefruit.setTxPower(3); // Max power is +4 dBm
-  // Bluefruit.setName("FeatherSS");
-  // Bluefruit.configPrphBandwidth(3);
-  
-  // Bluefruit.Periph.setConnectCallback(connect_callback);
-  // Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+  Serial.println("Setting up the bluetooth...");
+  setupBLE();
 
-  // Serial.println("Starting to broadcast service");
-
-  // // 3. FINALLY start the service
-  // imuService.begin();
-
-  // Setup Transmission Characterstic/Service
-  // 1. Setup properties
-  // txCharacteristic.setProperties(CHR_PROPS_NOTIFY);
-  // txCharacteristic.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  // txCharacteristic.setMaxLen(20);
-  // txCharacteristic.setCccdWriteCallback(cccd_callback);
-
-  // 2. Add characteristic to the service (this happens internally when you call begin)
-  // txCharacteristic.begin(); 
-
-  Serial.println("Setting up the receive characteristic...");
-
-  // Setup Receive Characteristic/Service
-  // rxCharacteristic.setProperties(CHR_PROPS_WRITE);
-  // rxCharacteristic.setPermission(SECMODE_OPEN, SECMODE_OPEN);
-
-  // // This tells the Feather: "Run 'onWriteCallback' when data arrives"
-  // rxCharacteristic.setWriteCallback(onWriteCallback);
-  // rxCharacteristic.begin();
-
-  // Start Advertising (makes the board visible to my laptop)
-  // startAdv();
+  startAdv();
 
   Serial.println("Attaching interrupt...");
   
@@ -101,27 +105,42 @@ void setup() {
 void startAdv(void) {
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addService(imuService);
-  Bluefruit.ScanResponse.addName();
-  Bluefruit.Advertising.start(0); // 0 = Don't stop advertising
+  Bluefruit.Advertising.start();
 }
+
+uint8_t testValue = 0;
 
 void loop() {
   if (triggerCalibration) {
     runCalibration();
     triggerCalibration = false; // Reset the flag
   }
-  if (Serial.available() > 0) {
-    char incomingByte = Serial.read(); // Read the key pressed
+  if (triggerSend){
+      if (Bluefruit.connected()) {
+        uint16_t conn_handle = Bluefruit.connHandle();
+        BLEConnection* conn = Bluefruit.Connection(conn_handle);
 
-    if (incomingByte == 'g' || incomingByte == 'G') {
+        while (conn->getMtu() < 100) {
+          conn->requestMtuExchange(247);
+          conn->requestConnectionParameter(6, 12);
+          Serial.println("Still at MTU 23... Requesting again...");
+          delay(1000); 
+          }
+        Serial.println("Hit the johnson ayeeee");
+        }
       sendData();
-    }
+      // Serial.println("Trigger 'g' received! Starting IMU...");
+      triggerSend = false;
   }
 }
 
 void sendData(){
   int num_packets_sent = 0;
   Serial.println("START");
+  if (Bluefruit.connected()) {
+    const char* startMsg = "START";
+    txCharacteristic.notify((uint8_t*)startMsg, strlen(startMsg));
+  }
   while (num_packets_sent < 1500){
     if (triggerRead) {
       //Serial.println("Getting the interrupt");
@@ -136,33 +155,74 @@ void sendData(){
       myData.gz = IMU.gyrZ();
       myData.time_stamp = currentMicros;
 
-      char buffer[50]; 
-
       Serial.print(myData.time_stamp);
-      Serial.print(" ");
+      Serial.print(" ,");
       Serial.print(myData.ax);
-      Serial.print(" ");
+      Serial.print(" ,");
       Serial.print(myData.ay);
-      Serial.print(" ");
-      Serial.print(myData.az);
-      Serial.print(" ");
+      Serial.print(" ,");
+      Serial.println(myData.az);
+      Serial.print(" ,");
       Serial.print(myData.gx);
-      Serial.print(" ");
+      Serial.print(" ,");
       Serial.print(myData.gy);
-      Serial.print(" ");
-      Serial.println(myData.gz);
+      Serial.print(" ,");
+      Serial.println(myData.gz);      
+
+      // Added lines for filling packet to be send
+      // Put into buffer
+      uint8_t newSample[SAMPLE_SIZE];
+      memcpy(dataBuffer + (sampleCount * SAMPLE_SIZE), &myData, sizeof(myData));
+      sampleCount++;
+
+      // If buffer is full (196 bytes) send data
+      if (sampleCount >= SAMPLES_PER_PACKET) {
+        if (Bluefruit.connected()) {
+          txCharacteristic.notify(dataBuffer, sizeof(dataBuffer));
+        }
+        sampleCount = 0; 
+      }
 
       triggerRead = false; // Reset the flag
       num_packets_sent += 1;
     }
   }
   Serial.println("END");
+  if (Bluefruit.connected()) {
+    const char* endMsg = "END";
+    txCharacteristic.notify((uint8_t*)endMsg, strlen(endMsg));
+  }
   return;
 }
 
 void connect_callback(uint16_t conn_handle) {
+  
+
+  BLEConnection* conn = Bluefruit.Connection(conn_handle);
+  
+  // 1. Request larger MTU (Max 247)
+  conn->requestMtuExchange(247);
+
+  Serial.print("Negotiated MTU: ");
+  Serial.println(conn->getMtu());
+  
+  // 2. Request Data Length Extension (DLE)
+  conn->requestDataLengthUpdate();
+  
+  // 3. Request fastest connection interval (7.5ms)
+  conn->requestConnectionParameter(6); // 6 * 1.25ms = 7.5ms
+
+  Serial.print("Connection Interval: ");
+  Serial.print(conn->getConnectionInterval() * 1.25);
+  Serial.println(" ms");
+
+  conn->requestPHY();
+
+  Serial.print("PHY Speed: ");
+  Serial.println(conn->getPHY() == BLE_GAP_PHY_2MBPS ? "2 Mbps" : "1 Mbps");
+
   Serial.print(millis());
-  Serial.println(" ms: BLE connected");
+  Serial.println(" ms: Connection established - Requested high-speed parameters");
 }
 
 void disconnect_callback(uint16_t conn_handle, uint8_t reason) {
@@ -225,4 +285,7 @@ void onWriteCallback(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, u
       // Logic to stop streaming
     }
   }
+}
+void receive_callback(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uint16_t len) {
+  triggerSend = true;
 }
